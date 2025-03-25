3ee7b4e659a86abbb3adfbb1dff846eb4e623fba
Pedro Lucas <pedrolucasluckow@hotmail.com>
2025-03-24 21:50:03 -0300

    #001 - add telas iniciais

---
+++ front-end/src/views/CadastrarView.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="about">
+    <RouterLink to="/">
+      <img src="../assets/imgs/Cart.svg" alt="Logo">
+    </RouterLink>
+    <h2>Gerenciador de Listas</h2>
+    <v-form>
+      <v-text-field label="Email" class="input mt-8"></v-text-field>
+      <v-text-field label="Usuário" class="input"></v-text-field>
+      <v-text-field label="Senha" class="input" type="password"></v-text-field>
+      <v-text-field label="Confirmar senha" class="input" type="password"></v-text-field>
+    </v-form>
+    <v-btn class="btn mt-8">Cadastrar</v-btn>
+    <p class="mt-4">Já possui uma conta? <RouterLink to="/login" class="ml-2">Entre</RouterLink> </p>
+  </div>
+</template>
+
+<style scoped>
+* {
+  font-family: "Roboto", sans-serif !important;
+}
+
+img {
+  height: 20vh;
+}
+
+.about {
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+}
+
+h2 {
+  font-size: 18px;
+}
+
+.input {
+  width: 75vw;
+  font-size: 16px;
+}
+
+.btn {
+  width: 75vw;
+  background-color: #0E8AFF;
+  color: white;
+}
+</style>
---
+++ front-end/src/views/HomeView.vue
@@ -0,0 +1,70 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <RouterView />
+  <header>
+    <div>
+      <div class="about">
+        <img src="../assets/imgs/Cart.svg" alt="Logo">
+        <h2>Gerenciador de Listas</h2>
+      </div>
+      <nav class="btns">
+        <RouterLink to="/login" class="mr-2 btn btn-1">Entrar</RouterLink>
+        <RouterLink to="/cadastrar" class="ml-2 btn btn-2">Cadastrar</RouterLink>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+* {
+  font-family: "Roboto", sans-serif !important;
+}
+
+img {
+  height: 25vh;
+}
+
+.about {
+  height: 75vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+}
+
+h2 {
+  font-size: 18px;
+  font-family: "Roboto", sans-serif !important;
+}
+
+.btns {
+  bottom: 0;
+  position: absolute;
+  padding-bottom: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  font-family: "Roboto", sans-serif !important;
+}
+
+.btn {
+  width: 40vw;
+  border-radius: 5px;
+  padding: 3vw;
+}
+
+.btn-1 {
+  border: 2px solid #0E8AFF;
+  background-color: white;
+  color: #0E8AFF;
+}
+
+.btn-2 {
+  background-color: #0E8AFF;
+  color: white;
+}
+</style>
